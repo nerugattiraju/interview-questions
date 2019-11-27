3b574d15c0d3efef34f4cc6b13f93d10c3843f01
raju <nerugattiraju123@gmail.com>
2019-11-27 16:35:00 +0530

    checking  primenumber or not

--- primenumbers.py
+++ primenumbers.py
@@ -6,3 +6,11 @@ for i in range(start,end+1):
             break
     else:
         print(i)
+print("check the given prime number of not")
+n=int(input())
+for i in range(2,i):
+    if n%i==0:
+        print('this is not prime number')
+        break
+else:
+    print("this is prime number")
